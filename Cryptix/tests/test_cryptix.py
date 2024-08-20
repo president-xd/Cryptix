76107b086ff6eb4989bec4a6eaa5d264a9b2be78
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2024-08-21 03:49:07 +0500

    Update test_cryptix.py

--- Cryptix/tests/test_cryptix.py
+++ Cryptix/tests/test_cryptix.py
@@ -1,5 +1,5 @@
 import unittest
-from Cryptix import Cryptix
+from ..cryptix import Cryptix
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
